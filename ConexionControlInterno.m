a490f80ffb46f66a03c59737b41e26a5cf300807
FranciscoBarCa <franbarragan87@gmail.com>
2022-06-05 16:22:23 +0200

    Limpieza del Simulink
    Se ha hecho un ensayo y limpiado el simulink

---
+++ ConexionControlInterno.m
@@ -0,0 +1,37 @@
+function [NoError] = ConexionControlInterno(DOMAIN_ID)
+%%%%%%%%%%%%%%%%%%%%%%%%
+%Lectura de constantes
+setenv("ROS_DOMAIN_ID",string(DOMAIN_ID))
+fprintf("Domain ID ROS2: %i \n",DOMAIN_ID)
+setenv("RMW_IMPLEMENTATION","rmw_cyclonedds_cpp")
+fprintf("Middleware: %s \n","rmw_cyclonedds_cpp")
+%Comprobación de la conexion /turtlebot3_node
+fprintf("Leyendo lista de nodos:\n")
+if any(contains(ros2("node","list"),'/turtlebot3_node'))
+    fprintf("    - Buena conexión con el robot. Nodo turtlebot3_node corriendo\n")
+else
+    fprintf("Error: No se encuentra el turtlebot3_node Asegurese que el robot está encendido y la configuración de domain es correcta\n")
+    NoError = 0;
+    return
+end
+%Comprobación de la conexion /EnsayoControlVelocidad
+fprintf("Despliegue del control:\n")
+if any(contains(ros2("node","list"),'/EnsayoControlVelocidad'))
+    fprintf("    - Nodo '/EnsayoPolosInterno' ya estaba desplegado.\n")
+else
+    d = ros2device('localhost');
+    runNode(d,'EnsayoControlVelocidad')
+    fprintf("    - Control desplegado.\n")
+    pause(10)
+    if any(contains(ros2("node","list"),'/EnsayoControlVelocidad'))
+        fprintf("    - Nodo '/EnsayoControlVelocidad' creado\n")
+    else
+        fprintf(" Error: Nodo '/EnsayoControlVelocidad' no encontrado en ROS2\n")  
+        NoError = 0;
+        return
+    end
+end
+fprintf("Fin de pruebas.\n")
+NoError = 1;
+return
+end
